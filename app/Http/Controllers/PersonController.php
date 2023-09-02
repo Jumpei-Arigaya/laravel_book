7076f913a01b003b4138d26c74a650464eeac7ab
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-09-02 16:23:55 +0900

    Eloquant ORM

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -20,8 +20,57 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
+    }
+
+    public function add(Request $requetst)
+    {
+        return view('person.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = new Person;
+        $form = $request->all();
+
+        // CSRFトークン情報を削除
+        unset($form['_token']);
+
+        // まとめてモデルに代入
+        $person->fill($form)->save();
+        return redirect('/person');
+    }
+
+    public function edit(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.edit', ['form' => $person]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = Person::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+        return redirect('/person');
+    }
+
+    public function delete(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.del', ['form' => $person]);
+    }
+
+    public function remove(Request $request)
+    {
+        Person::find($request->id)->delete();
+        return redirect('/person');
     }
 }

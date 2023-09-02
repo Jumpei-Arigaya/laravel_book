d89ba5d8ffdd5b2544c4beafa12a55af0b66a909
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-09-02 23:10:09 +0900

    has One

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.helloapp')
 
-@section('title', 'Person.index')
+@section('title', 'Board.index')
 
 @section('menubar')
     @parent
-    インデックスページ
+    ボード・ページ
 @endsection
 
 @section('content')
     <table>
         <tr>
-            <th>Person</th>
-            <th>Board</th>
+            <th>Data</th>
         </tr>
         @foreach ($items as $item)
             <tr>
                 <td>{{ $item->getData() }}</td>
-                <td>
-                    @if ($item->board != null)
-                        {{ $item->board->getData() }}
-                    @endif
-                </td>
             </tr>
         @endforeach
     </table>
+@endsection
+
+@section('footer')
+    copyright 2020 tuyano.
 @endsection
